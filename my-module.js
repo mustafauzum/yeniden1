859d1582c6a3e9ff4b30b98b72b38a4f8a4277b8
mustafauzum <mustafauzum05@gmail.com>
2024-05-02 00:44:57 +0300

    reactwork

---
+++ my-module.js
@@ -0,0 +1,23 @@
+export default name => {
+    console.log(`hello ${name}`);
+};
+
+const topla = (a, b) => a + b;
+const cikar = (a, b) => a - b;
+
+const text = 'Text';
+const user = {
+    name: "Ertugrul",
+    surname: "Uzum",
+};
+const users = [{
+    name: 'Emrecan',
+    surname: 'Uzum',
+}, {
+    name: 'Ahmet',
+    surname: 'Can',
+}]
+
+export {topla, cikar, text, user, users}
+
+
